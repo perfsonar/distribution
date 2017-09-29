26a07ab1b522ad4bfe5586eec156827119454d96
Antoine Delvaux <antoine.delvaux@gmail.com>
2017-09-29 14:46:35 +0000

    Automatically refresh quilt patches when releasing a Debian package.

--- debian/make-release.sh
+++ debian/make-release.sh
@@ -21,6 +21,7 @@ show_help() {
             -d: don't update distribution submodule
             -m: releases a minor package
             -n: performs a dry-run
+            -q: don't refresh quilt patches
             -t: additional git options to be passed to `git tag`
             -v: verbose
 EOF
@@ -42,6 +43,7 @@ verbose() {
 v=0
 dry_run=0
 minor_pkg=0
+quiltrefresh=1
 updatedistribution=1
 
 # Parsing options
@@ -52,6 +54,7 @@ while getopts "ac:dmnt:v" OPT; do
         d) updatedistribution=0 ;;
         m) minor_pkg=1 ;;
         n) dry_run=1 ;;
+        q) quiltrefresh=0 ;;
         t) tag_options=$OPTARG ;;
         v) 
             v=1
@@ -137,7 +140,7 @@ fi
 PS_DEB_REP="perfsonar-${BUILD_DISTRO}-${REL}"
 # We can have UNRELEASED as distro (we will change it later on), or it must be the correct $PS_DEB_REP
 if [[ "$CH_DISTRO" = "UNRELEASED" || "$CH_DISTRO" = "$PS_DEB_REP" ]]; then
-    verbose "The distribution filed in the debian/changelog file looks good: $CH_DISTRO."
+    verbose "The distribution field in the debian/changelog file looks good: $CH_DISTRO."
 else
     error "The distribution field in the debian/changelog file should be: $PS_DEB_REP (or UNRELEASED)"
 fi
@@ -157,13 +160,27 @@ verbose "The package signature line will be:"
 verbose "We're now going to release \033[1;32m${PKG}\033[0m at \033[1;32m${PKG_VERSION}\033[0m for \033[1;32m${PS_DEB_REP}\033[0m to the local git repo."
 verbose "This release will be tagged as \033[1;32m${DEBIAN_TAG}\033[0m."
 if [[ $updatedistribution -eq 1 ]]; then
-    verbose "We will also update the distribution submodule to latest commit on master."
+    verbose "We will update the distribution submodule to latest commit on master."
+fi
+if [[ $quiltrefresh -eq 1 && -d debian/patches ]]; then
+    verbose "We will try to refresh quilt patches to latest merge."
 fi
 if [[ $dry_run -eq 1 ]]; then
     v=1
     verbose "\033[1mThis is a dry run, I haven't touch a thing.\033[0m"
     exit
 fi
+
+# Refresh quilt patches if needed (if a refresh is not possible, this will stop and manual correction should happen)
+if [[ $quiltrefresh -eq 1 && -s debian/patches/series ]]; then
+    verbose "Trying to refresh quilt patches to latest merge."
+    QUILT_PATCHES="debian/patches"
+    QUILT_REFRESH_ARGS="-p ab --no-timestamps --no-index"
+    quilt --quiltrc - push -aq --refresh
+    quilt pop -aq
+    git add debian/patches
+fi
+
 # Actually change the debian/changelog file
 n=`grep -nm 1 " -- " debian/changelog | awk -F ':' '{print $1}'`
 TMP_FILE=`mktemp`
@@ -171,6 +188,7 @@ sed "${n}s/^ -- .* [+-][0-9]\{4\}/${FINISH_LINE}/" debian/changelog > $TMP_FILE
 sed "1s/ UNRELEASED;/ $PS_DEB_REP;/" $TMP_FILE > debian/changelog
 /bin/rm $TMP_FILE
 git add debian/changelog
+
 if [[ $updatedistribution -eq 1 ]]; then
     # Update the distribution submodule to latest master commit
     verbose "Updating the distribution submodule to latest commit on master."
@@ -184,6 +202,7 @@ if [[ $updatedistribution -eq 1 ]]; then
     cd ..
 fi
 git add distribution
+
 # And perform the commit and the tagging
 git commit ${commit_a} ${commit_options} -m "Releasing ${PKG} (${PKG_VERSION})"
 git tag ${DEBIAN_TAG}
