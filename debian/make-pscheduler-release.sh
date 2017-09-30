3d40380211a115bc15583bc11a60d817762bb9ba
Antoine Delvaux <antoine.delvaux@gmail.com>
2017-09-30 15:28:33 +0000

    Adding a script to help releasing pscheduler Debian packages.

---
+++ debian/make-pscheduler-release.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+# Usage info
+show_help() {
+    cat << EOF
+
+    Usage: ${0##*/} [-admnqv] [-c commit_options] [-t tag_options]
+
+    This script makes a pscheduler release. It loops on all pscheduler Debian packages
+    and check if the package is in a releasable state by calling make-release.sh
+
+    If all goes well, a new commit and a tag are made and added to the repository.
+
+    You can call this script with the following args:
+        -a: add modified files to the commit (use \`git commit -a\`)
+        -c: additional git options to be passed to \`git commit\`
+        -d: don't update distribution submodule
+        -n: performs a dry-run
+        -t: additional git options to be passed to \`git tag\`
+        -v: verbose
+EOF
+}
+
+# Error handler
+error() {
+    echo -e "\033[1m$1\033[0m" >&2
+    [ $v -eq 0 ] || echo -e "\033[1;31mBetter I stop now, before doing any commit to the local repo.\033[0m" >&2
+    exit 1
+}
+
+# Verbose handler
+verbose() {
+    [ $v -eq 0 ] || echo -e $1
+}
+
+# Defaults
+v=0                     # no verbose
+vout="/dev/null"        # no output
+mr_n=""                 # no dry run
+mr_m=""                 # not a minor-package
+update_distribution=1   # update distribution submodule
+
+# Parsing options
+while getopts "ac:dnt:v" OPT; do
+    case $OPT in
+        a) commit_a="-a" ;;
+        c) commit_options=$OPTARG ;;
+        d) update_distribution=0 ;;
+        n)
+            dry_run=1
+            mr_n="-n"
+            ;;
+        t) tag_options=$OPTARG ;;
+        v) 
+            v=1
+            vout="/dev/stdout"
+            mr_v="-v"
+            verbose "\033[1mI'm running in verbose mode.\033[0m" ;;
+        '?')
+            show_help >&2
+            exit 1 ;;
+    esac
+done
+shift $((OPTIND-1))
+
+# Loop on all pscheduler packages (new packages would need to be added here)
+for p in pscheduler-* python-pscheduler; do
+    case "$p" in
+        "pscheduler-rpm")
+            verbose "$p doesn't exist for Debian, we won't release it."
+            continue
+            ;;
+        "python-pscheduler")
+            pp="pscheduler"
+            mr_m="-m"
+            ;;
+        *)
+            pp=$p
+            ;;
+    esac
+
+    cd $p
+    if [ -d debian ]; then
+        upper_dir=".."
+    else
+        # Check if the Debian package is in a subdirectory
+        cd $pp
+        if [ -d debian ]; then
+            upper_dir="../.."
+        else
+            error "$pp doesn't look like a package I can release, maybe you need to correct the list of packages?"
+        fi
+    fi
+    # Run make-release.sh to check if everything is right for the current package
+    verbose "Checking $p is ready for release."
+    if ! ${upper_dir}/distribution/debian/make-release.sh $mr_v $mr_n $mr_m -d -g > $vout; then
+        error "I cannot release pscheduler because of $p"
+    fi
+    cd ${upper_dir}
+done
+
+# Get the final tag from pscheduler-bundle-full
+cd pscheduler-bundle-full
+OUT=`../distribution/debian/make-release.sh -d -g | tail -1`
+VERSION=${OUT%% *}
+TAG=${OUT##* }
+cd ..
+verbose ""
+echo "All looks fine, I will release pscheduler ${VERSION} with tag ${TAG}"
+if [[ $dry_run -eq 1 ]]; then
+    v=1
+    if [[ $update_distribution -eq 1 ]]; then
+        verbose "We will update the distribution submodule to latest commit on master."
+    fi
+    verbose "\033[1mReady to release \033[1;32mpscheduler ($VERSION)\033[1;39m with tag \033[1;32m$TAG\033[0m, but this is a dry run, I haven't touch a thing."
+    exit
+fi
+
+if [[ $update_distribution -eq 1 ]]; then
+    # Update the distribution submodule to latest master commit
+    verbose "Updating the distribution submodule to latest commit on master."
+    cd distribution
+    git checkout -q master
+    if [[ $v -eq 0 ]]; then
+        git pull -q
+    else
+        git pull
+    fi
+    cd ..
+    git add distribution
+fi
+
+# Perform the git commit and add the tag
+echo
+git commit ${commit_a} ${commit_options} -m "Releasing pscheduler (${VERSION})"
+git tag ${tag_options} ${TAG}
+echo
+echo "If you're happy with the commit and tag above, you just need to push that away!"
--- debian/make-release.sh
+++ debian/make-release.sh
@@ -24,6 +24,7 @@ show_help() {
         -a: add modified files to the commit (use \`git commit -a\`)
         -c: additional git options to be passed to \`git commit\`
         -d: don't update distribution submodule
+        -g: don't do any git action (no commit, no tag)
         -m: releases a minor package
         -n: performs a dry-run
         -q: don't refresh quilt patches
@@ -35,7 +36,7 @@ EOF
 # Error handler
 error() {
     echo -e "\033[1m$1\033[0m" >&2
-    echo -e "\033[1;31mBetter I stop now, before doing any commit to the local repo.\033[0m" >&2
+    [ $v -eq 0 ] || echo -e "\033[1;31mBetter I stop now, before doing any commit to the local repo.\033[0m" >&2
     exit 1
 }
 
@@ -47,19 +48,21 @@ verbose() {
 # Defaults
 v=0
 dry_run=0
+no_git=0
 minor_pkg=0
-quiltrefresh=1
-updatedistribution=1
+quilt_refresh=1
+update_distribution=1
 
 # Parsing options
-while getopts "ac:dmnt:v" OPT; do
+while getopts "ac:dgmnt:v" OPT; do
     case $OPT in
         a) commit_a="-a" ;;
         c) commit_options=$OPTARG ;;
-        d) updatedistribution=0 ;;
+        d) update_distribution=0 ;;
+        g) no_git=1 ;;
         m) minor_pkg=1 ;;
         n) dry_run=1 ;;
-        q) quiltrefresh=0 ;;
+        q) quilt_refresh=0 ;;
         t) tag_options=$OPTARG ;;
         v) 
             v=1
@@ -75,7 +78,7 @@ shift $((OPTIND-1))
 if [[ -f debian/changelog && -f debian/gbp.conf ]]; then
     verbose "debian/changelog and debian/gbp.conf are present, that's a good start."
 else
-    error "This doesn't look like a Debian packaging tree, I cannot find debian/changelog or debian/gbp.conf."
+    error "${PWD##*\/} doesn't look like a Debian packaging tree, I cannot find debian/changelog or debian/gbp.conf."
 fi
 
 # Check Debian branch
@@ -147,7 +150,7 @@ PS_DEB_REP="perfsonar-${BUILD_DISTRO}-${REL}"
 if [[ "$CH_DISTRO" = "UNRELEASED" || "$CH_DISTRO" = "$PS_DEB_REP" ]]; then
     verbose "The distribution field in the debian/changelog file looks good: $CH_DISTRO."
 else
-    error "The distribution field in the debian/changelog file should be: $PS_DEB_REP (or UNRELEASED)"
+    error "The distribution field in the debian/changelog of ${PWD##*\/} should be: $PS_DEB_REP (or UNRELEASED)"
 fi
 
 # Replace debian/changelog signature line with commiter or DEBIAN_EMAIL info
@@ -165,10 +168,10 @@ verbose "The package signature line will be:"
 echo -e "We're now going to release \033[1;32m${PKG}\033[0m at \033[1;32m${PKG_VERSION}\033[0m for \033[1;32m${PS_DEB_REP}\033[0m to the local git repo."
 echo -e "This release will be tagged as \033[1;32m${DEBIAN_TAG}\033[0m."
 if [[ $dry_run -eq 1 ]]; then
-    if [[ $quiltrefresh -eq 1 && -d debian/patches ]]; then
+    if [[ $quilt_refresh -eq 1 && -d debian/patches ]]; then
         verbose "We will try to refresh quilt patches to latest merge."
     fi
-    if [[ $updatedistribution -eq 1 ]]; then
+    if [[ $update_distribution -eq 1 ]]; then
         verbose "We will update the distribution submodule to latest commit on master."
     fi
     v=1
@@ -177,7 +180,7 @@ if [[ $dry_run -eq 1 ]]; then
 fi
 
 # Refresh quilt patches if needed (if a refresh is not possible, this will stop and manual correction should happen)
-if [[ $quiltrefresh -eq 1 && -s debian/patches/series ]]; then
+if [[ $quilt_refresh -eq 1 && -s debian/patches/series ]]; then
     verbose "Trying to refresh quilt patches to latest merge."
     QUILT_PATCHES="debian/patches"
     QUILT_REFRESH_ARGS="-p ab --no-timestamps --no-index"
@@ -186,7 +189,7 @@ if [[ $quiltrefresh -eq 1 && -s debian/patches/series ]]; then
     git add debian/patches
 fi
 
-if [[ $updatedistribution -eq 1 ]]; then
+if [[ $update_distribution -eq 1 ]]; then
     # Update the distribution submodule to latest master commit
     verbose "Updating the distribution submodule to latest commit on master."
     cd distribution
@@ -197,8 +200,8 @@ if [[ $updatedistribution -eq 1 ]]; then
         git pull
     fi
     cd ..
+    git add distribution
 fi
-git add distribution
 
 # Actually change the debian/changelog file
 n=`grep -nm 1 " -- " debian/changelog | awk -F ':' '{print $1}'`
@@ -208,10 +211,15 @@ sed "1s/ UNRELEASED;/ $PS_DEB_REP;/" $TMP_FILE > debian/changelog
 /bin/rm $TMP_FILE
 git add debian/changelog
 
-echo
-# And perform the commit and the tagging
-git commit ${commit_a} ${commit_options} -m "Releasing ${PKG} (${PKG_VERSION})"
-git tag ${DEBIAN_TAG}
+if [[ $no_git -eq 0 ]]; then
+    # Perform the git commit and add the tag
+    echo
+    git commit ${commit_a} ${commit_options} -m "Releasing ${PKG} (${PKG_VERSION})"
+    git tag ${tag_options} ${DEBIAN_TAG}
+    echo
+    echo "If you're happy with the commit and tag above, you just need to push that away!"
+else
+    # Or return the version and the tag
+    echo ${PKG_VERSION} ${DEBIAN_TAG}
+fi
 
-echo
-echo "If you're happy with the commit and tag above, you just need to push that away!"
