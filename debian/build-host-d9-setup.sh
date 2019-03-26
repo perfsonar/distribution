201688fc14d03e0761227159329434bd42a6681c
Antoine Delvaux <antoine.delvaux@gmail.com>
2019-03-26 12:36:57 +0000

    Generalizing build-host-d9-setup.sh so that it can be used to setup the Jenkins build host too.

--- debian/build-host-d9-setup.sh
+++ debian/build-host-d9-setup.sh
@@ -1,38 +1,39 @@
-#!/bin/sh
-# This script will setup a basic Debian installation to have everything needed to 
-# build perfSONAR Debian packages.
-# This script should be run as root, a few additional commands need to be run as regular user
-# Output is printed in white, so that it stands out from the green output of Vagrant
+#!/usr/bin/env bash
+# This script will augment a basic Debian 9 install to have everything needed to 
+# build perfSONAR Debian packages.  It should be run as root.
 
-# Get repo/package name
+# Get repo/package name and build root of shared repo
+[ -v host_pwd ] || host_pwd=`pwd`
 host_repo_path=${host_pwd%/distribution/debian}
-export PS_SHARED_REPO=/vagrant${host_repo_path##*/}
+export PS_SHARED_REPO=${SHARED_REPO_PREFIX}${host_repo_path}
+echo -e "\033[1;36mPreparing build environment with scripts from $PS_SHARED_REPO\033[0m"
 
-[ $http_proxy ] && echo "\033[1;36mUsing $http_proxy as http_proxy\033[0m"
-echo "\033[1;36mPreparing build environment with scripts from $PS_SHARED_REPO\033[0m"
+# Do we need to use a http_proxy?
+[ $http_proxy ] && echo -e "\033[1;36mUsing $http_proxy as http_proxy\033[0m"
 
 # Configure APT
 if [ -d ${PS_SHARED_REPO}/distribution/debian/build-host-files/apt.conf.d ]; then
-    echo "\033[1;36mConfiguring APT with files from build-host-files/apt.conf.d\033[0m"
+    echo -e "\033[1;36mConfiguring APT with files from build-host-files/apt.conf.d\033[0m"
     cp -R ${PS_SHARED_REPO}/distribution/debian/build-host-files/apt.conf.d/ /etc/apt/apt.conf.d/
 fi
 
 # Add contrib and backport repositories
-echo "\033[1;36mAdding contrib and backports repositories.\033[0m"
+echo -e "\033[1;36mAdding contrib and backports repositories.\033[0m"
 sed -i 's| main$| main contrib|' /etc/apt/sources.list
-if ! grep "stretch-backports main contrib" /etc/apt/sources.list; then
+if ! grep -q "stretch-backports main contrib" /etc/apt/sources.list; then
     echo "deb http://deb.debian.org/debian stretch-backports main contrib" >> /etc/apt/sources.list
 fi
 
 # Add a repository mirror if defined and make it the prefered source
-if [ -n "$MIRROR" ]; then
-    echo "\033[1;36mConfiguring Debian repository mirror.\033[0m"
+if ! grep -q "$MIRROR" /etc/apt/sources.list; then
+    echo -e "\033[1;36mConfiguring Debian repository mirror.\033[0m"
     cp ${PS_SHARED_REPO}/distribution/debian/build-host-files/sources.list.d/local-repo.list /etc/apt/sources.list.d/
     sed -i "s|::MIRROR::|${MIRROR}|" /etc/apt/sources.list.d/local-repo.list
-#    cat /etc/apt/sources.list >> /etc/apt/sources.list.d/local-repo.list
-#    mv /etc/apt/sources.list.d/local-repo.list /etc/apt/sources.list
+    cat /etc/apt/sources.list >> /etc/apt/sources.list.d/local-repo.list
+    mv /etc/apt/sources.list.d/local-repo.list /etc/apt/sources.list
     export MIRROR="${MIRROR}"
 fi
+
 # Do we need to build for some specific architectures only?
 [ -n "$ARCHES" ] && export ARCHES="${ARCHES}"
 
@@ -45,16 +46,14 @@ apt-get autoremove -y
 
 # Setup build environment
 mkdir -p /var/cache/pbuilder/hook.d/
+echo "# empty file" > /var/cache/pbuilder/hook.d/C99empty
 cp ${PS_SHARED_REPO}/distribution/debian/build-host-files/pbuilderrc /root/.pbuilderrc
-cp ${PS_SHARED_REPO}/distribution/debian/build-host-files/pbuilder-hook.d/* /var/cache/pbuilder/hook.d/
 cp ${PS_SHARED_REPO}/distribution/debian/build-host-files/scripts/cowbuilder-setup /root/
-chmod +x /root/cowbuilder-setup
-chmod +x /var/cache/pbuilder/hook.d/*
 
 # Create cowbuilder chroot
 for distro in jessie stretch; do
     export DIST="${distro}"
     /root/cowbuilder-setup
-    chown -R vagrant:vagrant /var/cache/pbuilder/result/${DIST}
+    chmod 777 /var/cache/pbuilder/result/$DIST
 done
 
--- debian/vagrant-build-host-d9-user-setup.sh
+++ debian/vagrant-build-host-d9-user-setup.sh
@@ -1,13 +1,17 @@
-#!/bin/sh
-# This script will setup a basic Debian installation to have everything needed to 
-# build perfSONAR Debian packages.
-# It is the normal user script part.
+#!/usr/bin/env bash
+# This script will finish the preparation of the Vagrant Debian build host
+# to have everything ready to build perfSONAR Debian packages.
+# It should be run as a normal user (i.e. vagrant).
 
-echo "\033[1;36mSetting up vagrant user.\033[0m"
+echo -e "\033[1;36mSetting up vagrant user environment.\033[0m"
 
-# Get repo/package name
+# Get repo/package name and build root of shared repo
+[ -v host_pwd ] || host_pwd=`pwd`
 host_repo_path=${host_pwd%/distribution/debian}
-export PS_SHARED_REPO=/vagrant/${host_repo_path##*/}
+export PS_SHARED_REPO=${SHARED_REPO_PREFIX}${host_repo_path}
+
+# Add useful pbuilder hooks
+sudo cp ${PS_SHARED_REPO}/distribution/debian/build-host-files/pbuilder-hook.d/* /var/cache/pbuilder/hook.d/
 
 # Adding pS building scripts
 cp ${PS_SHARED_REPO}/distribution/debian/build-host-files/pbuilderrc /home/${USER}/.pbuilderrc
@@ -15,19 +19,19 @@ cp ${PS_SHARED_REPO}/distribution/debian/build-host-files/scripts/ps-cowbuilder-
 chmod +x /home/${USER}/ps-cowbuilder-build
 
 # Make sure result directory in the Vagrant share is existing
-if [ ${PS_SHARED_REPO} = "/vagrant/" ]; then
+if [ "${PS_SHARED_REPO}" = "/vagrant" ]; then
     RESULT_DIR=${PS_SHARED_REPO}/result
 else
     RESULT_DIR=${PS_SHARED_REPO}/../result
 fi
-[ -d ${RESULT_DIR} ] || mkdir -p ${RESULT_DIR}
+[ -d ${RESULT_DIR} ] || mkdir -p ${RESULT_DIR}
 
 # Install user dotfiles if supplied
 # See https://github.com/tonin/osx.dotfiles for example
 # Beware that is you deploy your own ssh public keys, you'll need to tweak vagrant.ssh configuration
 # For example, with config.ssh.keys_only = false
-if [ -n ${VAGRANT_USER_DOTFILES} ]; then
-    echo "\033[1;36mCloning dotfiles user repo.\033[0m"
+if [ -n "${VAGRANT_USER_DOTFILES}" ]; then
+    echo -e "\033[1;36mCloning dotfiles user repo.\033[0m"
     mkdir -p /home/${USER}/.config
     if [ ! -d /home/${USER}/.config/dotfiles ]; then
         git clone ${VAGRANT_USER_DOTFILES} /home/${USER}/.config/dotfiles
@@ -36,7 +40,7 @@ if [ -n ${VAGRANT_USER_DOTFILES} ]; then
         git pull
     fi
 fi
-if [ -n ${VAGRANT_USER_DOTFILES_DEPLOY} ]; then
-    echo "\033[1;36mDeploying user dotfiles.\033[0m"
+if [ -n "${VAGRANT_USER_DOTFILES_DEPLOY}" ]; then
+    echo -e "\033[1;36mDeploying user dotfiles.\033[0m"
     /home/${USER}/.config/dotfiles/${VAGRANT_USER_DOTFILES_DEPLOY}
 fi
