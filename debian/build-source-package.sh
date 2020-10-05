3b3cf5e0427a157252f33a49f23c5c79b2043c5e
Antoine Delvaux <antoine.delvaux@gmail.com>
2020-10-05 16:01:29 +0000

    Simplifiying the way the UPSTREAM_TAG is derived from the Debian tag, all is configured in gbp.conf

--- debian/build-source-package.sh
+++ debian/build-source-package.sh
@@ -99,15 +99,7 @@ if [ -z $DEBIAN_TAG ]; then
 else
     # If we have a tag, we take the source from the git tag
     echo -e "\nBuilding release package of ${PKG} from ${DEBIAN_TAG}.\n"
-    # We build the upstream tag from the Debian tag by, see https://github.com/perfsonar/project/wiki/Versioning :
-    # - adding a leading "v"
-    # - removing the leading debian/distro prefix
-    # - removing the ending -1 debian-version field
-    # - transforming any ~x.b1 beta relnum to -x.b1
-    UPSTREAM_TAG=v${DEBIAN_TAG##*\/}
-    UPSTREAM_TAG=${UPSTREAM_TAG%-*}
-    UPSTREAM_TAG=${UPSTREAM_TAG/~/-}
-    GBP_OPTS="$GBP_OPTS --git-upstream-tree=$UPSTREAM_TAG"
+    # The upstream tag will be derived from the gbp.conf config and rules from https://github.com/perfsonar/project/wiki/Versioning :
     # We don't sign the release package as we don't have the packager's key
     dpkgsign="-us -uc"
 fi
